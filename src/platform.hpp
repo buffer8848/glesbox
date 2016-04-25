710220b9706c00473c61b8aac702f666b36c2837
Li, Binfei <binfeix.li@intel.com>
2016-04-25 15:36:58 +0800

    build for linux and android

--- src/platform.hpp
+++ src/platform.hpp
@@ -29,6 +29,11 @@
 #include <sys/time.h>
 #include <unistd.h> 
 #include <fcntl.h>
+#include <string.h>
+#include <memory.h>
+#include "GLES2/gl2.h"
+#include "GLES2/gl2ext.h"
+#include "EGL/egl.h"
 #endif
 
 //-------------------------------------------------------------------------------------------------
